3ae7c526acfea16f6ea9a633d348cf9605023b09
mohamdii <m.hamdy97@gmai.com>
2023-06-26 07:53:25 +0000

    3

--- printf.c
+++ printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdarg.h>
+#include <string.h>
 /**
  * _printf - creates a printf like funtion
  * @format: the string
@@ -9,6 +10,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int count = 0;
 
 	va_start(args, format);
 	while (*format != '\0')
@@ -22,14 +24,16 @@ int _printf(const char *format, ...)
 			{
 				char *s = va_arg(args, char*);
 				print_string(s);
+				count += strlen(s);
 			}
 			if (*format == 'c')
 			{
 				int c = va_arg(args, int);
 				_putchar(c);
+				count++;
 			}	
 		}
 		format++;
 	}
-	return (1);
+	return (count);
 }
